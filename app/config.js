f559c4f83fe104be69cc5cda8acfa1a4eef93ce8
Oceanne Esparcieux <oesparcieux@companieshouse.gov.uk>
2022-01-14 10:38:43 +0000

    get certs internal system commit

--- app/config.js
+++ app/config.js
@@ -4,7 +4,7 @@
 
 module.exports = {
   // Service name used in header. Eg: 'Renew your passport'
-  serviceName: 'Payment analysis tool',
+  serviceName: 'Internal systems',
 
   // Default port that prototype runs on
   port: '3002',
